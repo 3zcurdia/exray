6f3177638ec31464d9a9b8b9b23f99b2b70384f3
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2025-01-20 22:27:59 -0600

    Add experimental concurrency with async streams

--- lib/exray.ex
+++ lib/exray.ex
@@ -24,4 +24,36 @@ defmodule Exray do
 
     %PPM{ppm | pixels: pixels}
   end
+
+  # Experimental, concurrent rendering
+  def render_concurrent(width \\ 256, height \\ 256) do
+    ppm = PPM.new(width, height)
+
+    ppm
+    |> render_pixels_concurrent()
+    |> PPM.write("hello_concurrent.ppm")
+  end
+
+  defp render_pixels_concurrent(%PPM{width: width, height: height} = ppm) do
+    max_workers = System.schedulers_online() - 1
+    rows = height - 1
+
+    pixels =
+      0..(rows - 1)
+      |> Task.async_stream(
+        fn j ->
+          row =
+            for i <- 0..(width - 1), into: [] do
+              "#{Color.new(i / (width - 1), j / (height - 1), 0.0)}\n"
+            end
+
+          {j, row}
+        end,
+        max_concurrency: max_workers
+      )
+      |> Enum.sort_by(fn {:ok, {index, _data}} -> index end)
+      |> Enum.map(fn {:ok, {_index, chunk}} -> chunk end)
+
+    %PPM{ppm | pixels: pixels}
+  end
 end
